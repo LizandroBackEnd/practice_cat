5e4b2d7817d32d746d5e84901eae049d8186dc17
Lizandro Antonio <codeversebackend@gmail.com>
2024-03-07 10:05:13 -0600

    Create proyect with React Native

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import {Audio} from  'expo-av';
 
 export default function App() {
+  const  image = "https://www.nationalgeographic.com.es/medio/2023/08/25/perro_adf93861_1412331876_230825124420_800x800.jpg"
+    const [sound , setSound] = useState()
+
+  async function playSong(){
+    const {sound} = await Audio.Sound.createAsync (require ("./dog.mp3"))
+
+      setSound(sound)
+      await sound.playAsync();
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Image source={{uri: image}} style={{width: 200, height: 200}}/>
+        <Button title="Escuchar" onPress={playSong}/>
+        <StatusBar style="auto"/>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+      gap: 5,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
